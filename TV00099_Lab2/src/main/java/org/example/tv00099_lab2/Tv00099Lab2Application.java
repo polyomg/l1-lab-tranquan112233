20aad022586bccdeb6811b6d44fbb2a4a1537a72
TranHoangQuan <tranquan567098@gmail.com>
2025-10-02 22:52:38 +0700

    TV00099-Trần Hoàng Quân-Lab1,2,3

---
+++ TV00099_Lab2/src/main/java/org/example/tv00099_lab2/Tv00099Lab2Application.java
@@ -0,0 +1,13 @@
+package org.example.tv00099_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tv00099Lab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tv00099Lab2Application.class, args);
+	}
+
+}
